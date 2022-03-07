d991133b8d9b8057777145f5513f05ff6047f02e
Dobek <pauldobe@38f9d3c2313c.ant.amazon.com>
2022-03-07 18:24:21 -0500

    This is a new change

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        This is a test application by Dobek
+
       </header>
     </div>
   );
